5d20afcb359192330388551fcb74ec2f4bdeca73
YorwosBanana <yorwos@windowslive.com>
2016-03-13 19:38:44 +0200

    καινουριο Gannt

--- temp/13-3 ολα σε ενα html/functions.js
+++ temp/13-3 ολα σε ενα html/functions.js
@@ -1,15 +1,25 @@
 //CHARACTER
 
+
 //CLASS
 
+
 //SKILLS
+
+
+//PROFILE
+function show_Profile(){
+	document.getElementById("Map_field").style.display = "none";
+	document.getElementById("Battle_field").style.display = "none";
+	document.body.style.backgroundColor = "#FF99CC";
+	document.getElementById("Profile_field").style.display = "";
+}
 
 //LEVELING
 var ex_level = "";
 var experience = 0;
-
 function exp_Points(){
-	experience = experience + (ex_level*50/* επι  bonus*/);
+	experience = experience + (ex_level*67/* επι  bonus*/);
 }
 function level_up(){
 	player["player_experience"] += experience;
@@ -35,18 +45,78 @@ function level_up(){
 	}else if(player['level'] >= 90 && player['level'] <= 99){
 		lv_x = 512;
 	}
+	document.getElementById('level_g').innerHTML = "<br>Experience gained: " + experience
+	
 	if(player["player_experience"] >= lv_x * 1500){
+		while(player["player_experience"] >= lv_x * 1500){
 			player['level'] += 1;
 			experience = 0;
-			document.getElementById('level_g').innerHTML = "Level: " + player["level"];
+			player["player_experience"] -= lv_x * 1500;
+			document.getElementById('level_g').innerHTML += "<br>Level: " + player["level"];
 		}
-	if(experience != 0){
-		document.getElementById('level_g').innerHTML = "Experience gained: " + experience;
+	}else{
+		document.getElementById('level_g').innerHTML += "<br>Level: " + player["level"];
 	}
-
+	//if(experience != 0){
+	//	document.getElementById('level_g').innerHTML += "<br>Experience gained: " + experience + "<br>" + "Experience: " + player["player_experience"] + "/" + lv_x * 1500;
+	//}
+	document.getElementById('level_g').innerHTML += "<br>" + "Experience: " + player["player_experience"] + "/" + lv_x * 1500;
+	document.getElementById('level_g').style.visibility = "visible";
 }
 
 //BATTLE
+function show_Battle(){
+	document.getElementById("Profile_field").style.display = "none";
+	document.getElementById("Map_field").style.display = "none";
+	initBattle();
+	document.body.style.backgroundColor = "#AAAAAA";
+	document.getElementById("Battle_field").style.display = "";
+}
+function initBattle(){
+	battle = "not over";
+	round_Moves = 10;
+	counter = 1;
+	experience = 0;
+	
+	log = "";
+	document.getElementById('battle_log').innerHTML = log; 
+	document.getElementById("turn").innerHTML = "Round(s): " + counter;
+	
+	document.getElementById("battle_buttons").style.visibility = "visible";
+	
+	document.getElementById("next_Phase_button").style.visibility = "hidden";
+	
+	document.getElementById('level_g').style.visibility = "hidden";
+	
+	document.getElementById('player_info').style.visibility = "visible";
+	document.getElementById("current_player_stamina_bar").style.visibility = "visible";
+	document.getElementById("current_player_health_bar").style.visibility = "visible";
+	document.getElementById("current_player_mana_bar").style.visibility = "visible";
+			
+	document.getElementById("enemy1_info").style.visibility = "visible";
+	document.getElementById("enemy1").style.visibility = "visible";
+	document.getElementById("current_enemy1_health_bar").style.visibility = "visible";
+	enemylife1 = "alive";
+	
+	document.getElementById("enemy2_info").style.visibility = "visible";
+	document.getElementById("enemy2").style.visibility = "visible";
+	document.getElementById("current_enemy2_health_bar").style.visibility = "visible";
+	enemylife2 = "alive";
+	
+	current_player_health = player["health"];
+	current_player_stamina = player["stamina"];
+	current_player_mana = player["mana"];
+
+	current_enemy1_health = enemy["health"];
+	current_enemy1_stamina = enemy["stamina"];
+	current_enemy1_mana = enemy["mana"];
+
+	current_enemy2_health = enemy2["health"];
+	current_enemy2_stamina = enemy2["stamina"];
+	current_enemy2_mana = enemy2["mana"];
+	Stats();
+	seconds = 60;
+}
 var player ={name:"Player",attack:50,manaAttack:20,stamina:50,health: 200,mana: 50,level: 1,player_experience: 0};//Αυτά θα ειναι του Αποστόλη
 var enemy ={name:"Enemy1",attack:50,manaAttack:20,stamina:50,health: 250,mana: 50,level: 1};//Τυχαία με for/random για πανω απο ενα
 var enemy2 ={name:"Enemy2",attack:40,manaAttack:20,stamina:60,health: 400,mana: 50,level: 1};//Δεν θα υπάρχει καθόλου, θα ειναι στο απο πανω με for
@@ -80,7 +150,8 @@ function next_Turn(){//Τέλος του γύρου του παίκτη-παίζ
 	}
 	if (current_player_health <= 0){
 		battle = "over";
-		window.location.href = "profile.html";
+		show_Profile();
+		//window.location.href = "profile.html";
 	}
 	end_Round_log();
 	damage_dealt = 0;
@@ -119,16 +190,27 @@ function Stats(){//Ενημέρωση των στοιχειων στην οθό�
 	document.getElementById('current_player_stamina_bar').setAttribute("width", ps + "px");
 	document.getElementById('current_player_mana_bar').setAttribute("width", pm + "px");
 	
-	var e1h = (200*current_enemy1_health)/enemy["health"] ;
-	document.getElementById('current_enemy1_health_bar').setAttribute("width", e1h + "px");
-	
-	var e2h = (200*current_enemy2_health)/enemy2["health"] ;
-	document.getElementById('current_enemy2_health_bar').setAttribute("width", e2h + "px");
+	if(enemylife1 != "dead"){
+		var e1h = (200*current_enemy1_health)/enemy["health"] ;
+		document.getElementById('current_enemy1_health_bar').setAttribute("width", e1h + "px");
+	}else{
+		var e1h = 0 ;
+		document.getElementById('current_enemy1_health_bar').setAttribute("width", e1h + "px");
+	}
+	if(enemylife2 != "dead"){
+		var e2h = (200*current_enemy2_health)/enemy2["health"] ;
+		document.getElementById('current_enemy2_health_bar').setAttribute("width", e2h + "px");
+	}else{
+		var e2h = 0 ;
+		document.getElementById('current_enemy2_health_bar').setAttribute("width", e2h + "px");
+	}
 	
 	var e1stats='<img id= "enemy1_image"src="default_avatar.gif" alt="default avatar" style="width:100px;height:100px;">'.concat("<br>",enemy["name"]," Level: ",enemy["level"]);
-	document.getElementById('enemy1_info').innerHTML=e1stats;
+	document.getElementById('enemy1_info').innerHTML=e1stats; 
+	 
 	var e2stats='<img id= "enemy2_image"src="default_avatar.gif" alt="default avatar" style="width:100px;height:100px;">'.concat("<br>",enemy2["name"]," Level: ",enemy2["level"]);
 	document.getElementById('enemy2_info').innerHTML=e2stats;
+	
 	var pstats='<img id= "player_image"src="default_avatar.gif" alt="default avatar" style="width:100px;height:100px;">'.concat("<br>",player["name"]," Level: ",player["level"], " Moves: ",round_Moves);
 	document.getElementById('player_info').innerHTML=pstats;
 }
@@ -219,12 +301,20 @@ function damage_Calculation(){
 			document.getElementById("current_player_health_bar").style.visibility = "hidden";
 			document.getElementById("current_player_mana_bar").style.visibility = "hidden";
 			document.getElementById("battle_buttons").style.visibility = "hidden";
-			document.getElementById("profile_button").style.visibility = "visible";
+			document.getElementById("next_Phase_button").style.visibility = "visible";
 			level_up();
 		}
 	}
 }
 
+function next_Phase(){
+	if(traveling == true){
+		show_Map();
+	}
+	else{
+		show_Profile();
+	}
+}
 function run_from_Battle(){
 	var x = Math.floor((Math.random() * 10) + 1);
 	if (((x == 4 || x == 5 || x == 8) && player["stamina"]>=40)||x == 4){
@@ -240,10 +330,16 @@ function secondPassed() {
     if (remainingSeconds < 10) {
         remainingSeconds = "0" + remainingSeconds;  
     }
-    document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
+	var countdown = document.getElementById("Battle_field").style.display;
+	if (countdown  != "none") {
+		document.getElementById("countdown").innerHTML = minutes + ":" + remainingSeconds;
+	}
     if (seconds == 0) {
 		if (battle == "not over"){
-			next_Turn();
+			//var countdown = document.getElementById('countdown');
+			if (countdown != "none") {
+				next_Turn();
+			}
 		}
     } else {
         seconds--;
@@ -253,7 +349,83 @@ function secondPassed() {
 var countdownTimer = setInterval('secondPassed()', 1000);
 
 //MAP
+/*var newwindow;
+	function popup(url)
+	{
+		newwindow=window.open(url,'name','height=100px', 'width=1000px');
+		if (window.focus) {newwindow.focus()}
+	}*/
+	
+function show_Map(){
+	document.getElementById("Profile_field").style.display = "none";
+	document.getElementById("Battle_field").style.display = "none";
+	document.body.style.backgroundColor = "#000033";
+	if(backToX != 0 && backToY != 0)
+	{
+		playerX = backToX;
+		playerY = backToY;
+		var elem = document.getElementById("moving_player");
+		elem.style.top = playerY + 'px'; 
+		elem.style.left = playerX + 'px';
+		
+		document.getElementById("Map_field").style.display = "";
+		myMove(backCountX,backCountY);
+	}else{
+		document.getElementById("Map_field").style.display = "";
+	}
+	//document.getElementById("Map_field").style.display = "";
+}
+var playerX = 700;
+var playerY = 700;
+var backToX = 0;
+var backToY = 0;
+var backCountX =0;
+var backCountY = 0;
+var traveling = false;
+	
+function myMove(countryX,countryY) {
+	var elem = document.getElementById("moving_player");
+	var id = setInterval(frame, 20);
+	function frame() {
+		if (countryY == playerY && countryX == playerX) {
+			backToX = 0;
+			backToY = 0;
+			traveling = false;
+			clearInterval(id);
+		} else {
+			if(countryX > playerX){
+				playerX += 1;
+			}else if(countryX < playerX){
+				playerX -= 1;
+			}
+			if(countryY > playerY){
+				playerY += 1;
+			}else if(countryY < playerY){
+				playerY -= 1;
+			}
+			//land_Check();
+			elem.style.top = playerY + 'px'; 
+			elem.style.left = playerX + 'px'; 
+			var rBattle = Math.floor((Math.random() * 100) + 1);
+			if (rBattle == 2){
+				backToX = playerX;
+				backToY = playerY;
+				backCountX = countryX;
+				backCountY = countryY;
+				traveling = true;
+				show_Battle();
+				clearInterval(id);
+			}
+		}
+	}
+}
 
+//function land_Check(){
+	//if(playerX && playerY)
+	//{
+		
+	//}
+//}
 //QUESTS
 
 //CHAT?
